7b30d171cf511ddebf8215dcb0bcd434197928f6
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-27 18:30:30 -0300

    feat(ap01-mss): implementa endpoints post e get de lembretes

--- mss/lembretes/src/app.ts
+++ mss/lembretes/src/app.ts
@@ -1,2 +1,50 @@
-let hello: string = "Hello, Typescript";
-console.log(hello)
+import express from 'express'
+const app = express()
+app.use(express.json())
+/*
+  {
+    "1": {
+      "id": "1",
+      "texto": "Fazer café"
+    },
+    "2": {
+      "id": "2",
+      "texto": "Nadar"
+    }
+  }
+*/
+interface Lembrete {
+  id: string;
+  texto: string;
+}
+const lembretes: Record <string, Lembrete> = {}
+let id: string = '1'
+//GET /lembretes obter a coleção de lembretes
+app.get('/lembretes', (req, res) => {
+  res.json(lembretes)
+})
+
+
+//POST /lembretes cadastrar novo lembrete
+app.post('/lembretes', (req, res) => {
+  //extrair o texto do corpo da requisição:  {"texto": "Fazer café"}
+  const { texto } = req.body
+  //construir um novo lembrete
+  const lembrete = { id, texto }
+  //armazenar o novo lembrete
+  lembretes[id] = lembrete
+  //incremento o id
+  id = (+id + 1).toString()
+  //responder ao cliente
+  res.json(lembrete)
+})
+
+//GET /lembretes/{id} obter um lembrete pelo id
+//pesquisar como podemos pegar o id dado que ele faz parte da url
+app.get('', (req, res) => {
+  
+})
+
+
+const port = 4000
+app.listen(port, () => console.log(`Lembretes. Porta ${port}.`))
