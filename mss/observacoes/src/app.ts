d84fb61ea89cf7a7282bc4117795a5e2cb285bab
Rodrigo Bossini <professorbossini@gmail.com>
2024-04-03 15:49:31 -0300

    feat(ap-1-mss): encerra o codigo 4.3.6
    - define o endpoint de cadastro de observações
    - define o endpoint de obtenção de observações

--- mss/observacoes/src/app.ts
+++ mss/observacoes/src/app.ts
@@ -1,2 +1,19 @@
-let hello: string = "Hello, Typescript";
-console.log(hello)
+import express from 'express'
+const app = express()
+app.use(express.json())
+
+
+//POST /lembretes/123456/observacoes
+app.post('/lembretes/:id/observacoes', (req, res) => {
+
+})
+
+//GET /lembretes/123456/observacoes
+app.get('/lembretes/:id/observacoes', (req, res) => {
+
+})
+
+const port = 5000
+app.listen(port, () => {
+  console.log(`Observações. ${port}`)
+})
